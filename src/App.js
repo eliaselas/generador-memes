3b2cca22bb2fddeed446b428ff25dd5657c48c99
elias <hernandezelas@gmail.com>
2022-10-29 15:28:45 -0600

    Agregados estilos para Meme.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Header from './componentes/Header';
+import "./App.css";
+import Header from "./componentes/Header";
+import Meme from "./componentes/Meme";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,12 +3,16 @@ import "../hojas-de-estilo/Header.css";
 import trollface from '../imagenes/troll-face.png'
 
 export default function Header() {
+  const descriptionImagen = () => {
+    console.log("Este es la imagen de TROLLL-FACE")
+  }
   return (
     <header className="header">
       <img
         src={trollface}
         alt="esta es la imagen un troll-face"
         className='header--image'
+        onMouseOver={descriptionImagen}
       />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Elias</h4>
