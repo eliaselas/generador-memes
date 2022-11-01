081197d824ade68cb7902d03578062e7020bb83f
elias <hernandezelas@gmail.com>
2022-11-01 00:23:59 -0600

    Agregando alt a <img> para no tener error al compilar

--- src/componentes/Meme.js
+++ src/componentes/Meme.js
@@ -1,51 +1,39 @@
-import React, {useState} from 'react';
-import '../hojas-de-estilo/Meme.css';
-import memesData from '../memesData';
+import React, { useState } from "react";
+import "../hojas-de-estilo/Meme.css";
+import memesData from "../memesData";
 //Componente en el cual se creara el meme
 export default function Meme() {
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg"
+    randomImage: "http://i.imgflip.com/1bij.jpg",
   });
   const [allMemeImages, setAllMemeImages] = useState(memesData);
 
   function getMemeImage(e) {
-    e.preventDefault()
+    e.preventDefault();
     const memesArray = allMemeImages.data.memes;
-    const randomNumber =  Math.floor(Math.random() * memesArray.length)
-    const url = memesArray[randomNumber].url
-    setMeme(prevMeme => ({
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
       ...prevMeme,
-      randomImage: url
-    }))
-
+      randomImage: url,
+    }));
   }
   return (
     <main>
       <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Get a new meme image 🖼
-                </button>
-            </div>
-            <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
-                <h2 className="meme--text top">One does not simply</h2>
-                <h2 className="meme--text bottom">Walk into Mordor</h2>
-            </div>
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" alt='Imagen meme '/>
+        <h2 className="meme--text top">One does not simply</h2>
+        <h2 className="meme--text bottom">Walk into Mordor</h2>
+      </div>
     </main>
-  )
+  );
 }
